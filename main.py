c566dabf8c6dbc541ca86f210b1b2fbb8d3faeac
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-18 06:00:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Write a Python program to print the prime numbers for a user provided range
+start = int(input())
+end = int(input())
+for i in range(start, end+1):
+
+    if i > 1:
+
+        for j in range(2, i):
+
+            if(i % j == 0):
+
+                break
+
+        else:
+
+            print(i)
+ 
